89ef620c1de0ae23f2a7da3062b6025a101221c8
milestarricone <milestarricone2@gmail.com>
2024-10-27 22:19:57 -0400

    found the rest of the lab after number 16. finished now

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const pokemon = require(`./data.js`)
+const pokemon = require("./data.js");
+
 
 const game = {
     party: [],
@@ -310,3 +311,179 @@ game.gyms.forEach(gym => {
 })
 game.gymStatus()
 
+/*
+Exercise 17
+1. Arrange the Pokémon in `game.party` by their HP. The one with the highest HP should come first.
+2. You'll need to use the `.sort()` method. How does the compare function work in sorting numbers?
+
+
+Solve Exercise 17 here:
+*/
+
+game.party.sort((pokemon1, pokemon2) => pokemon2.hp - pokemon1.hp);//documentation was not very helpful from MDM. went to w3 schools and Microsoft copilot for better explinations
+console.log(game.party)
+
+
+/*
+Exercise 18
+Add a new property to the `game` object called `collection` and initialize its value to an empty array.
+
+Copy the `catchPokemon` method you wrote in Exercise Twelve and paste it below. Modify it so that:
+  - Ensure that no more than six Pokemon can be in the party at any time. 
+    Excess Pokemon should be placed in the `game.collection` array.
+  - It's up to you how to distribute Pokemon in a situation where more than six 
+    would be placed into the `game.party` array.
+
+Again, for this exercise, it's okay to have a negative number of pokeballs.
+
+After updating the method, use it by calling it and passing in a pokemon object of your choice from the `pokemon` data to catch it.
+
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 18 here:
+*/
+
+
+game.collection = []
+
+game.catchPokemon = function(pokemonObj) {
+  // 
+  if (game.party.length >= 6) {
+    game.collection.push(pokemonObj)
+  }
+  if (game.party.length < 6) {
+    this.party.push(pokemonObj)
+  }
+  const pokeball = this.items.find(item => item.name === `pokeball`);
+  // game.items.filter(item => item.name === `pokeball` I dont know why the filter did not wortk. but I got it by using an if statement. 
+      // item.pokeball.quantity - 1   
+  if (pokeball.quantity > 0) {
+      pokeball.quantity -= 1;
+  }   
+}
+
+game.catchPokemon(pokemon.find(pokemon => pokemon.number === 145))
+console.log(game.collection);
+console.log(game.items);
+
+
+/*
+Exercise 19
+Copy the `catchPokemon` method that you just wrote above, and paste it below. The time has come to make it so that we cannot catch a Pokemon when we do not have any pokeballs to catch it with. 
+
+Modify the method so that if there are no pokeballs a message will be displayed that there are not enough pokeballs to catch the desired Pokemon.
+
+Also, ensure that the Pokemon isn't added to the `game.party` or the `game.collection`.
+
+Solve Exercise 19 here:
+*/
+
+game.catchPokemon = function(pokemonObj) {
+  //
+  const pokeball = this.items.find(item => item.name === `pokeball`);
+  if (pokeball.quantity !== 0) { 
+    if (game.party.length >= 6) {
+      game.collection.push(pokemonObj)
+    }
+    if (game.party.length < 6) {
+      this.party.push(pokemonObj)
+    }
+    // const pokeball = this.items.find(item => item.name === `pokeball`);
+    // game.items.filter(item => item.name === `pokeball` I dont know why the filter did not wortk. but I got it by using an if statement. 
+        // item.pokeball.quantity - 1   
+    if (pokeball.quantity > 0) {
+        pokeball.quantity -= 1;
+    }   }
+  else {
+    console.log(`you have to Pokeballs. You should have stocked up you terrible gamer. `);
+    
+  }
+}
+
+
+/*
+Exercise 20
+Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify is so that you can just pass in the name of a Pokemon instead of an entire object, and the method will look up the Pokemon from the data set for you.
+
+The string passed in should be allowed to be any case (for example, if the string 'PiKacHU' is passed to the function, it should match to 'Pikachu' in the data set). 
+
+If there is not a match, then return a string noting that the selected Pokemon does not exist. Ensure you do not decrement the pokeball count if an invalid Pokemon name is passed in, and also ensure that the Pokemon isn't added to the `game.party` or the `game.collection`.
+
+Solve Exercise 20 here:
+*/
+
+game.catchPokemon = function(pokemonName) {
+  //
+  const pokeball = this.items.find(item => item.name === `pokeball`);//find the pokeball
+  if (pokeball.quantity !== 0) { //check if pokeball is available
+
+    const pokemonObj = pokemon.find(pokemon => pokemon.name.toLowerCase() === pokemonName.toLowerCase());////this took me forever to figure out. And I needed help from microsoft copilot to get the syntax right. But I am comparing a case insensative version of the name the user is passing to a case insensative version of the name from the array.
+    if (!pokemonObj) {
+      console.log('The selected Pokémon does not exist.');
+      return 'The selected Pokémon does not exist.';
+  }
+    if (game.party.length >= 6) {//check if you have room in the party
+      game.collection.push(pokemonObj)
+      if (pokeball.quantity > 0) {
+        pokeball.quantity -= 1;
+    }
+    }
+    else if (game.party.length < 6) {
+      this.party.push(pokemonObj)
+      if (pokeball.quantity > 0) {
+        pokeball.quantity -= 1;
+    }
+    }
+    // const pokeball = this.items.find(item => item.name === `pokeball`);
+    // game.items.filter(item => item.name === `pokeball` I dont know why the filter did not wortk. but I got it by using an if statement. 
+        // item.pokeball.quantity - 1   
+       }
+  else {
+    console.log(`you have to Pokeballs. You should have stocked up you terrible gamer. `);
+    
+  }
+}
+game.catchPokemon(`Muk`)
+game.catchPokemon(`okahfhoi`)
+
+console.log(game.collection);
+
+
+/*
+Exercise 21
+Dynamically construct an object with the existing `pokemon` data sorted by the different pokemon types. The object will have this structure:
+
+{
+  grass: [
+    { number: 1, name: 'Bulbasaur', type: 'grass', hp: 45, starter: true },
+    { number: 2, name: 'Ivysaur', type: 'grass', hp: 60, starter: false },
+    { number: 3, name: 'Venusaur', type: 'grass', hp: 80, starter: false },
+    * more grass type Pokemon objects...
+  ],
+  fire: [
+    { number: 4, name: 'Charmander', type: 'fire', hp: 39, starter: true },
+    * more fire type Pokemon objects...
+  ],
+  water: [
+    * water type Pokemon objects...
+  ],
+  * etc... until there is an array for every Pokemon type!
+}
+
+Log the object when it's constructed.
+
+Solve Exercise 21 here:
+*/
+
+const pokemonByType = {};////needed a lot of assistance from Microsoft copilot AI to complete this part. I understand how it works now, but I could not have completed this without assistance from AI. 
+
+for (let i = 0; i < pokemon.length; i++) {
+  let pokemonForNewArray = pokemon[i];
+  let type = pokemonForNewArray.type;
+
+  if(!pokemonByType[type]) {
+    pokemonByType[type] = []
+  }
+  pokemonByType[type].push(pokemonForNewArray);
+}
+console.log(pokemonByType)
